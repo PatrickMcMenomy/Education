bce7137ea337e40fd29ca75ce19dad28076f8023
Patrick McMenomy <Patrick.mcmenomy@gmail.com>
2017-03-21 17:52:54 -0600

    Added the ability to get the salt automatically.

--- CS50/crack/crack.c
+++ CS50/crack/crack.c
@@ -6,23 +6,28 @@
 #include <cs50.h>
 
 string password;
-string hash;
+string hash; // This is the stored password needing cracked
+char salt[2];
 
 int main (int argc, string argv[]) {
   if (argc == 2) {
-    hash = argv[1];
 
+    // Store the encrypted password in hash and generate salt. 
+    hash = argv[1];
+    salt[0] = hash[0];
+    salt[1] = hash[1];
+    printf("salt is %s!\n", salt);
+
+    // Check to see if hash is already decrypted. 
     if ( hash != password) {
       
-      for (int i = 0; i < 256; i++) {
-        crypt("aaaa", "50");
-      }
-      printf("%s\n", password);
+      // Crack password.
+      
     }
-  }
   else {
     printf("Usage: ./crack hash\n");
     return 1;
   }
   return 0;
+  }
 }
